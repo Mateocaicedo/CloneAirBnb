9cb328588b2f86ed81855be9f549d1b7a52bd4ef
Mateocaicedo <caicedomateo9@gmail.com>
2022-04-13 17:44:47 -0500

    vista principal y login

---
+++ app/src/main/java/com/grupoParcial/airbnbclone/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.grupoParcial.airbnbclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupoParcial.airbnbclone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.iniciarsesion)
+
+    }
+}
